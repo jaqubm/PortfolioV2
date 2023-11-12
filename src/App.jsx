b15ca3116f81e087358789bbbafea7c20fc6af57
jaqubm <jaqobgm@gmail.com>
2023-11-12 19:42:13 +0100

    Repo init

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className='h-screen w-screen flex flex-wrap content-center justify-center bg-background'>
+      <h1 className='text-5xl font-bold text-text'>
+        portfolio_v2 0.0.1
+      </h1>
+    </div>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,33 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{html,js,jsx}"],
+  theme: {
+    colors: {
+      'text': '#ffffff',
+      'background': '#020517',
+      'primary': '#0aff9d',
+      'secondary': '#f2f2f2',
+      'accent': '#a985ff',
+     },     
+     fontSize: {
+      sm: '0.750rem',
+      base: '1rem',
+      xl: '1.333rem',
+      '2xl': '1.777rem',
+      '3xl': '2.369rem',
+      '4xl': '3.158rem',
+      '5xl': '4.210rem',
+    },
+    fontFamily: {
+      heading: 'Poppins',
+      body: 'Poppins',
+    },
+    fontWeight: {
+      normal: '400',
+      bold: '700',
+    },    
+    extend: {},
+  },
+  plugins: [],
+}
+
