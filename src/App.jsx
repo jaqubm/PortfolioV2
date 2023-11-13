a2968019c01e4a2a2758f183f4c1832c50a6f7b6
jaqubm <jaqobgm@gmail.com>
2023-11-13 21:18:01 +0100

    'Work in Progress' Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
+import React from 'react'
+
+import TestImage from './assets/space_05.svg'
+
 import './App.css'
 
 function App() {
   return (
-    <div className='h-screen w-screen flex flex-wrap content-center justify-center bg-background'>
+    <div className='h-screen w-screen flex flex-col flex-wrap content-center justify-center bg-background text-center'>
+      <img src={ TestImage } className='h-[500px]' />
       <h1 className='text-5xl font-bold text-text'>
-        portfolio_v2 0.0.1
+        BE RIGHT BACK SOON
       </h1>
     </div>
   )
