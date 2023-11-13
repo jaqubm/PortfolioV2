1773412546ea069b49880a434dfd969da23bf9cf
jaqubm <jaqobgm@gmail.com>
2023-11-13 21:18:37 +0100

    TYPO

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 
-import TestImage from './assets/space_05.svg'
+import WorkInProgressSVG from './assets/space_05.svg'
 
 import './App.css'
 
 function App() {
   return (
     <div className='h-screen w-screen flex flex-col flex-wrap content-center justify-center bg-background text-center'>
-      <img src={ TestImage } className='h-[500px]' />
+      <img src={ WorkInProgressSVG } className='h-[500px]' />
       <h1 className='text-5xl font-bold text-text'>
         BE RIGHT BACK SOON
       </h1>
