0020eee15f67912f82c8f410b1d8318c86cbcbfc
jaqubm <jaqobgm@gmail.com>
2023-11-18 13:22:43 +0100

    Setting up App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,12 @@
 import React from 'react'
 
-import WorkInProgressSVG from './assets/workInProgress.svg'
-
 import './App.css'
 
 function App() {
   return (
-    <div className='h-screen w-screen flex justify-center text-center'>
-      <div className='w-11/12 flex flex-col flex-wrap justify-center'>
-        <img src={ WorkInProgressSVG } className='h-[350px]' />
-        <div className='text-3xl font-black'>
-          BE RIGHT BACK SOON!
-        </div>
-        <div className='font-bold'>
-          working on something amazing...
-        </div>
+    <div className='flex justify-center'>
+      <div className='max-w-screen-xl'>
+        Content
       </div>
     </div>
   )
