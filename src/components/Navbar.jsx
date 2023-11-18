8d17d2480c1a9306bc5680a0ee70a6fbbe5c1aa1
jaqubm <jaqobgm@gmail.com>
2023-11-18 23:31:15 +0100

    Created simple Navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div className='w-full flex flex-row justify-center text-base'>
+      <div>
+        Home
+      </div>
+      <div>
+        About me
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
