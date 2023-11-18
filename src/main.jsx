29c02285f97adf5fd3844684dd54e992cb91117d
jaqubm <jaqobgm@gmail.com>
2023-11-18 14:47:38 +0100

    Deleted NextUI2

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,10 @@
 // main.tsx or main.jsx
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import {NextUIProvider} from '@nextui-org/react'
 import App from './App'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <NextUIProvider>
-      <App />
-    </NextUIProvider>
+    <App />
   </React.StrictMode>,
 )
