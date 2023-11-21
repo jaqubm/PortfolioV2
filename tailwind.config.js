b91cba499d5b8895e0691c13d81beeffc92c5664
jaqubm <jaqobgm@gmail.com>
2023-11-21 18:19:00 +0100

    Deleted nextUI

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,7 @@
-const { nextui } = require("@nextui-org/react");
-
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
     "./src/**/*.{html,js,jsx}",
-    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
     colors: {
@@ -36,19 +33,6 @@ export default {
     },    
     extend: {},
   },
-  darkMode: "class",
-  plugins: [nextui({
-    themes: {
-      light: {
-        colors: {
-          'text': '#ffffff',
-          'background': '#020517',
-          'primary': '#0aff9d',
-          'secondary': '#f2f2f2',
-          'accent': '#a985ff',
-        }
-      }
-    }
-  })]
+  plugins: []
 }
 
