cdc1986c3968e7f61ecb75b2b25184aa8f3a2688
jaqubm <jaqobgm@gmail.com>
2023-11-21 23:30:47 +0100

    Updated stylling

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -12,7 +12,7 @@ function HeroSection() {
   return(
     <div className='w-full flex sm:flex-row flex-col-reverse justify-center items-center'>
 
-      <div className='flex-auto w-8/12 flex flex-col items-start'>
+      <div className='flex-auto w-9/12 flex flex-col items-start'>
 
         <div className='lg:text-7xl text-5xl font-black [text-shadow:_10px_12px_0_rgb(169_133_255_/_100%)]'>
           Hey,
@@ -22,7 +22,7 @@ function HeroSection() {
           I'm Jakub Matuszak
         </div>
         
-        <div className='flex lg:text-2xl text-xl'>
+        <div className='flex lg:text-2xl text-xl lg:mt-[-24px] mt-[-18px]'>
           <div>
             I'm a&nbsp;
           </div>
@@ -37,7 +37,7 @@ function HeroSection() {
           </div>
         </div>
 
-        <div className='flex lg:flex-row flex-col lg:items-center items-start lg:space-x-4'>
+        <div className='flex sm:flex-row flex-col lg:items-center items-start sm:space-x-4'>
 
           <a href='https://www.linkedin.com/in/jaqubm/' target='_blank' className='lg:text-4xl text-2xl font-black lg:[text-shadow:_5px_6px_0_rgb(0_255_157_/_100%)] lg:hover:[text-shadow:_8px_10px_0_rgb(0_255_157_/_100%)] [text-shadow:_3px_4px_0_rgb(0_255_157_/_100%)]'>
             LinkedIn
