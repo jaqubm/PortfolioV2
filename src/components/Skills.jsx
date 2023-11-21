647e119aa91b430d527dbdc1d0ad085ad87e0076
jaqubm <jaqobgm@gmail.com>
2023-11-21 23:03:36 +0100

    Added Skills header

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -3,9 +3,11 @@ import React from 'react'
 function Skills() {
   return (
     <div className='w-full flex sm:flex-row flex-col-reverse justify-center items-center'>
-      <div>
+
+      <div className='lg:text-7xl text-4xl font-black [text-shadow:_10px_12px_0_rgb(169_133_255_/_100%)] lg:mb-0 mb-3'>
         Skills
       </div>
+
     </div>
   )
 }
