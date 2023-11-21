4d8cd9946c2d00a83d83912de5e2756dbff670ec
jaqubm <jaqobgm@gmail.com>
2023-11-21 17:08:15 +0100

    Updated stylling

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,17 +2,17 @@ import React from 'react'
 
 function Navbar() {
   return (
-    <div className='w-full flex flex-row justify-center space-x-4 mt-3 text-base font-bold'>
-      <div className='cursor-pointer hover:text-primary'>
+    <div className='w-full flex flex-row justify-center space-x-4 my-3 text-base font-bold'>
+      <div className='cursor-pointer hover:underline'>
         Home
       </div>
-      <div className='cursor-pointer hover:text-primary'>
+      <div className='cursor-pointer hover:underline'>
         About me
       </div>
-      <div className='cursor-pointer hover:text-primary'>
+      <div className='cursor-pointer hover:underline'>
         Projects
       </div>
-      <div className='cursor-pointer hover:text-primary'>
+      <div className='cursor-pointer hover:underline'>
         Contact me
       </div>
     </div>
