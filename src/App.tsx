f4384f0e66931859ba399c23594a8cbcb6639acf
jaqubm <jaqobgm@gmail.com>
2023-11-25 20:02:56 +0100

    Created Skills component

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,25 @@
 //Imports
 import Hero from "./components/pages/Hero"
 import AboutMe from "./components/pages/AboutMe"
+import Skills from "./components/pages/Skills"
 
 function App() {
   return (
     <>
     <div className="w-screen text-text flex justify-center">
-      <div className="w-full lg:block hidden max-w-[1100px] mx-10">
+      <div className="w-full lg:block hidden max-w-[1100px] mx-10 mb-10">
         <div className="flex flex-col justify-center">
 
           <Hero />
 
           <div className="flex space-x-10">
 
-            <div className="flex-1">
+            <div className="flex-1 flex flex-col space-y-10">
               <AboutMe />
+              <Skills />
             </div>
 
-            <div className="flex-1">
+            <div className="flex-1 flex flex-col space-y-10">
               Right
             </div>
 
@@ -26,11 +28,14 @@ function App() {
         </div>
       </div>
 
-      <div className="w-full lg:hidden block max-w-[340px] mx-6">
+      <div className="w-full lg:hidden block max-w-[340px] mx-6 mb-6">
         <div className="flex flex-col justify-center">
           <Hero />
 
-          <AboutMe />
+          <div className="space-y-4">
+            <AboutMe />
+            <Skills />
+          </div>
         </div>
       </div>
     </div>
---
+++ src/components/pages/Skills.tsx
@@ -0,0 +1,41 @@
+import BorderBox from "../BorderBox"
+import Title from "../Title"
+import Skill from "../Skill"
+
+const Skills = () => (
+  <>
+    <BorderBox>
+
+      <Title>
+        Skills
+      </Title>
+
+      <div className="space-y-7">
+
+        <Skill progress={ 50 }>
+          C# / .NET
+        </Skill>
+
+        <Skill progress={ 50 }>
+          .NET Core
+        </Skill>
+
+        <Skill progress={ 50 }>
+          C / C++
+        </Skill>
+
+        <Skill progress={ 50 }>
+          Java
+        </Skill>
+
+        <Skill progress={ 50 }>
+          OOP
+        </Skill>
+
+      </div>
+
+    </BorderBox>
+  </>
+)
+
+export default Skills
