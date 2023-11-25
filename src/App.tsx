a17a574d48454591c45d2ab271f85249149966d5
jaqubm <jaqobgm@gmail.com>
2023-11-25 16:39:25 +0100

    Updated stylling for Hero component for mobile

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <>
     <div className="w-screen text-text flex justify-center">
-      <div className="w-full lg:block hidden max-w-[1100px]">
+      <div className="w-full lg:block hidden max-w-[1100px] mx-10">
         <div className="flex flex-col justify-center">
           <Hero />
 
@@ -13,8 +13,10 @@ function App() {
         </div>
       </div>
 
-      <div className="lg:hidden block">
-        <div>
+      <div className="w-full lg:hidden block max-w-[320px] mx-7">
+        <div className="flex flex-col justify-center">
+          <Hero />
+
           Mobile
         </div>
       </div>
--- src/components/pages/Hero.tsx
+++ src/components/pages/Hero.tsx
@@ -1,21 +1,21 @@
 import HeroDecorations from "../../assets/HeroDecorations.svg"
 
 const Hero = () => (
-  <div className="flex flex-col justify-center mt-10 mb-20 pt-20 text-center relative">
+  <div className="flex flex-col justify-center lg:mt-10 mt-4 lg:mb-20 mb-9 lg:pt-20 pt-9 text-center relative">
 
-    <img src={ HeroDecorations } className="opacity-[.03] absolute m-auto top-0 left-0 right-0" />
+    <img src={ HeroDecorations } className="lg:max-w-none max-w-[540px] w-full opacity-5 absolute m-auto top-0 left-0 right-0" />
 
-    <div className="text-9xl font-black [text-shadow:_3px_12px_0px_rgba(10,_255,_157,_0.25);]">
+    <div className="lg:text-9xl text-6xl font-black lg:[text-shadow:_3px_12px_0px_rgba(10,_255,_157,_0.25);] [text-shadow:_1.3px_5.7px_0px_rgba(10,_255,_157,_0.25);]">
       Jakub
     </div>
-    <div className="mt-[-32px] mb-14 text-primary text-9xl font-black [text-shadow:_3px_12px_0px_rgba(255,_255,_255,_0.25);]">
+    <div className="lg:-mt-8 -mt-2 lg:mb-14 mb-7 text-primary lg:text-9xl text-6xl font-black lg:[text-shadow:_3px_12px_0px_rgba(255,_255,_255,_0.25);] [text-shadow:_1.3px_5.7px_0px_rgba(255,_255,_255,_0.25);]">
       Matuszak
     </div>
 
-    <div className="text-5xl font-bold relative">
+    <div className="lg:text-5xl text-xl font-bold relative">
       Software Engineer
 
-      <div className="text-[64px] opacity-[.03] font-black absolute m-auto top-4 left-0 right-0">
+      <div className="lg:text-[64px] text-[27px] opacity-5 font-black absolute m-auto lg:top-4 top-2.5 left-0 right-0">
         Software Engineer
       </div>
     </div>
