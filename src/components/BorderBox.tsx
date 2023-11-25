56f1e90dcda251ea4b20574141aaa5460f5d6f21
jaqubm <jaqobgm@gmail.com>
2023-11-25 19:19:57 +0100

    Updated BorderBox component

--- src/components/BorderBox.tsx
+++ src/components/BorderBox.tsx
@@ -1,11 +1,11 @@
 import { ReactNode } from "react"
 
-type Props = {
+type BorderBoxProps = {
   children: ReactNode
 }
 
-const BorderBox = ({children} : Props) => (
-  <div className="w-full bg-white rounded-[20px] px-5 py-2.5">
+const BorderBox = ({ children } : BorderBoxProps) => (
+  <div className="w-full bg-white rounded-[20px] px-5 pt-2.5 pb-4 lg:text-base text-sm">
     { children }
   </div>
 )
