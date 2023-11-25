f76638efd3836982cce60bd5201fbbb607f36e1f
jaqubm <jaqobgm@gmail.com>
2023-11-25 19:58:43 +0100

    Updated ProgressBar color

--- src/components/ProgressBar.tsx
+++ src/components/ProgressBar.tsx
@@ -5,7 +5,7 @@ type ProgressBarProps = {
 const ProgressBar = ({ progress } : ProgressBarProps) => (
   <>
     <div className="w-full h-5 bg-gray rounded-full relative">
-      <div className="h-5 bg-primary rounded-full absolute top-0 left-0" style={{ width: `${progress}%`}} />
+      <div className="h-5 bg-accent rounded-full absolute top-0 left-0" style={{ width: `${progress}%`}} />
     </div>
   </>
 )
