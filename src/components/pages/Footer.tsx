5bb0d2f7d4f7d5a1291bc24971e24790ce243592
jaqubm <jaqobgm@gmail.com>
2023-11-25 21:40:42 +0100

    Created Footer component

---
+++ src/components/pages/Footer.tsx
@@ -0,0 +1,9 @@
+const Footer = () => (
+  <>
+    <p className="lg:max-w-sm max-w-xs text-text-60 text-[10px] text-center">
+      Designed in <span className="text-text">Figma</span> and coded in <span className="text-text">Visual Studio Code</span> by Jakub Matuszak. Built with <span className="text-text">React.ts</span> and <span className="text-text">Tailwind CSS</span>, deployed with <span className="text-text">GitHub pages</span>.
+    </p>
+  </>
+)
+
+export default Footer
