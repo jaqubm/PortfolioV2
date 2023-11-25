8bf0312bbf9b02b05c0e8cec8775576aa3296f96
jaqubm <jaqobgm@gmail.com>
2023-11-25 19:28:38 +0100

    Updated AboutMe component

--- src/components/pages/AboutMe.tsx
+++ src/components/pages/AboutMe.tsx
@@ -6,12 +6,24 @@ const AboutMe = () => (
     <BorderBox>
 
       <Title>
-        Left
+        About me
       </Title>
 
       <p>
         I am a Software Engineer and C# / .NET Developer currently pursuing Engineer's Degree in Computer Science at Lodz University of Technology, a person who loves to code and get known with new technologies, but that's obvious. Beside that I am in love with PlayStation and playing video games. I really enjoy doing Calisthenics. I am also into Cars, mainly Porsche.
       </p>
+
+      <div className="flex space-x-5 font-medium mt-3">
+
+        <a className="hover:text-primary" href="https://www.linkedin.com/in/jaqubm/" target="_blank">
+          LinkedIn
+        </a>
+
+        <a className="hover:text-primary" href="https://github.com/jaqubm" target="_blank">
+          GitHub
+        </a>
+
+      </div>
 
     </BorderBox>
   </>
