79c1831b8778480f5d35138cd6d205d2ed2ce67b
jaqubm <jaqobgm@gmail.com>
2023-12-06 20:33:57 +0100

    Footer Update

--- src/components/pages/Footer.tsx
+++ src/components/pages/Footer.tsx
@@ -1,7 +1,7 @@
 const Footer = () => (
   <>
     <p className="lg:max-w-sm max-w-xs text-text-60 text-[10px] text-center">
-      Designed in <span className="text-text">Figma</span> and coded in <span className="text-text">Visual Studio Code</span> by Jakub Matuszak. Built with <span className="text-text">React.ts</span> and <span className="text-text">Tailwind CSS</span>, deployed with <span className="text-text">GitHub pages</span>.
+      Designed in <span className="text-text">Figma</span> and coded in <span className="text-text">Visual Studio Code</span> by Jakub Matuszak. Built with <span className="text-text">React</span>, <span className="text-text">TypeScript</span> and <span className="text-text">Tailwind CSS</span>, deployed with <span className="text-text">GitHub pages</span>.
     </p>
   </>
 )
