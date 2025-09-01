91c9203a764f654a70138f53205d93c8c62bf3c7
jaqubm <jaqobgm@gmail.com>
2025-09-01 18:40:03 +0200

    Updated certificates

--- src/components/Certifications.tsx
+++ src/components/Certifications.tsx
@@ -13,6 +13,38 @@ const Certifications = () => (
       <div className="space-y-5">
 
         <DetailInfo
+        date="JULY 2025"
+        link="https://learn.microsoft.com/api/credentials/share/en-us/jaqubm/1A5CF60B6A8C703F?sharingId"
+        title="Microsoft Certified: Azure Fundamentals"
+        company="Microsoft"
+        subtitle={["Credential ID 1A5CF60B6A8C703F"]}
+        isLink={ true } />
+
+        <DetailInfo
+        date="JULY 2025"
+        link="https://learn.kodekloud.com/certificate/8eaa021a-55c8-4555-9d77-8ac74ae828a9"
+        title="Udemy Labs - Online Kubernetes Lab for Beginners - Hands On"
+        company="KodeKloud"
+        subtitle={["Credential ID 8eaa021a-55c8-4555-9d77-8ac74ae828a9"]}
+        isLink={ true } />
+
+        <DetailInfo
+        date="JUNE 2025"
+        link="https://credentials.databricks.com/cadbece7-8eca-4792-b12e-b64c93f7e053"
+        title="Academy Accreditation - Databricks Fundamentals"
+        company="Databricks"
+        subtitle={["Credential ID 149354987"]}
+        isLink={ true } />
+
+        <DetailInfo
+        date="MAY 2025"
+        link="https://www.udemy.com/certificate/UC-43940342-92d1-4a20-b842-bbf4936948ac/"
+        title="C# .NET with MS SQL Complete Beginner to Master"
+        company="Udemy"
+        subtitle={["Credential ID UC-43940342-92d1-4a20-b842-bbf4936948ac"]}
+        isLink={ true } />
+
+        <DetailInfo
         date="MARCH 2024"
         link="https://www.udemy.com/certificate/UC-e65a92bf-b366-4613-b771-f8d0de19d20b/"
         title="Scala & Functional Programming Essentials"
